2c336654df40f58fb620a135eba0a5a3fd29e785
nilugoes <ana_goes@outlook.com>
2021-11-04 22:30:52 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import NavBar from "components/NavBar";
+import DataTable from "components/NavBar/DataTable";
+import Footer from "components/NavBar/Footer";
 import React from "react";
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
