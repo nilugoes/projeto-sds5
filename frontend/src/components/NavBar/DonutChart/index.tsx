ca426f1736a9ef5511fd216191fbbffc67f34195
nilugoes <ana_goes@outlook.com>
2021-11-07 00:10:51 -0300

    BarChart integration

--- frontend/src/components/NavBar/DonutChart/index.tsx
+++ frontend/src/components/NavBar/DonutChart/index.tsx
@@ -10,21 +10,21 @@ type ChartData = {
 }
 const DonutChart = () => {
 
-    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: []});
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
     useEffect(() => {
 
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[]; 
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum)
 
-            setChartData({ labels: myLabels, series: mySeries });
-            console.log(chartData);
-        });
-    } , []);
-   
+                setChartData({ labels: myLabels, series: mySeries });
+                console.log(chartData);
+            });
+    }, []);
+
 
     /*const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
@@ -37,14 +37,14 @@ const DonutChart = () => {
         }
     }
 
-return (
-    <Chart
-        options={{ ...options, labels: chartData.labels }}
-        series={chartData.series}
-        type="donut"
-        height="240"
-    />
-);
+    return (
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
+            type="donut"
+            height="240"
+        />
+    );
 }
 
 export default DonutChart;
--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,13 @@
+import { type } from "os"
+
 export type SaleSum = {
-    "sellerName": string,
-    "sum": number
+    "sellerName": string;
+    "sum": number;
 }
+
+export type SaleSuccess =  {
+
+    sellerName: string;
+    visited: number;
+    deals: number;
+};
