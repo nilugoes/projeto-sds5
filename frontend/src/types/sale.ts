891505c0a63429e1209ef6e9d574c4352fb8189f
nilugoes <ana_goes@outlook.com>
2021-11-07 00:57:52 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,5 +1,26 @@
-import { type } from "os"
+import { Seller } from "./seller";
 
+export type Sale =         {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+    
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size?: number;
+    first: boolean;
+    number: number;
+    numberOfElements?: number;
+    empty?: boolean;
+}
 export type SaleSum = {
     "sellerName": string;
     "sum": number;
